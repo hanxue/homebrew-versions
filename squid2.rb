1fd4d3b83d517e63e35c5f1851465995c9c5edb1
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-22 09:17:05 +0800

    Add SSL and plist for launchctl

--- squid2.rb
+++ squid2.rb
@@ -10,6 +10,31 @@ class Squid2 < Formula
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+                          "--enable-ssl"
     system "make install"
+  end
+  
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_prefix}/sbin/squid</string>
+        <string>-N</string>
+        <string>-d 1</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+    </dict>
+    </plist>
+    EOS
   end
 end
